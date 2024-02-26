a09b5ffca953e67bff6e24d6aae9485bbbd048e2
unknown <laothucatkhanhle@gmail.com>
2024-02-27 00:14:49 +0700

    Cap nhat bo sung ngay 27/2, thay doi lai css cac phan tu co chua nut xem them

--- Client/Pages/TrangChu/index.js
+++ Client/Pages/TrangChu/index.js
@@ -137,23 +137,3 @@ buttonCategory.addEventListener("click", () => {
         subMenu.style.display = "none";
     }
 })
-
-//Can chinh kich thuoc cac item co phan "xem them"
-const itemRow2 = document.querySelectorAll('.index-item-row2-boxs-text');
-let maxHeight = 0;
-
-for (let i = 0; i < itemRow2.length; i++) {
-    const height = itemRow2[i].clientHeight;
-    if (height > maxHeight) {
-        maxHeight = height;
-    }
-}
-
-for (let i = 0; i < itemRow2.length; i++) {
-    itemRow2[i].style.height = maxHeight + 'px';
-}
-
-window.addEventListener("resize", () => {
-    window.location.reload();
-})
-//Can chinh kich thuoc cac item co phan "xem them"
